3a704fd951d41c47c8f4370c312b8225944aec74
Вячеслав Блинков <xristian860810@gmail.com>
2022-08-11 22:55:58 +0300

    домашнее задание по теме принцыпы ООП , ч2 выполнено

---
+++ src/Bicycle.java
@@ -0,0 +1,14 @@
+public class Bicycle extends Transport {
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышки велосипеда");
+        System.out.println("Меняем покрышки велосипеда");
+
+    }
+
+    @Override
+    public void checkTransport() {
+        System.out.println("С велосипедом все в порядке");
+    }
+}
